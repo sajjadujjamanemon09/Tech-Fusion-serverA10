4c6473314716effecc78ce41e23edd644294bf2d
Sajjaduj Jaman Emon <mdsajjadujjaman09@gmail.com>
2023-10-20 03:36:12 +0600

    use delete methode to delete cart data form database

--- index.js
+++ index.js
@@ -114,6 +114,14 @@ async function run() {
 })
 
 
+// delete
+app.delete('/carts/:id', async (req, res) => {
+  const id = req.params.id;
+  const query = { _id: new ObjectId(id) };
+  const result = await cartsCollection.deleteOne(query);
+  res.send(result);
+})
+
 
 
     app.post('/product', async(req, res) => {
